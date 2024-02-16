3db2dfe2a2935ebd07d1f16175295d17ed538bb1
Cloud User <ec2-user@ip-172-31-28-92.us-west-2.compute.internal>
2024-02-16 15:55:44 +0000

    d

--- ECCO-ACCESS/Cloud_access_to_ECCO_datasets/benchmark_download.py
+++ ECCO-ACCESS/Cloud_access_to_ECCO_datasets/benchmark_download.py
@@ -7,30 +7,43 @@ import sys
 
 from ecco_s3_retrieve import *
 import time
+import multiprocessing
 
 if __name__ == '__main__':
+    print(f'CPU: {multiprocessing.cpu_count()}')
+    sd = '2000-01'
+    ed = '2010-12'
+    engine = 'h5netcdf'
+    print(f'Start: {sd}\nEnd: {ed}')
+    print(f'Engine: {engine}')
     if sys.argv[1] == 'dask-parallel':
         from dask.distributed import Client
 
-        client = Client("tcp://127.0.0.1:39333")
+        client = Client()
         print(client)
+        st = time.time()
 
         file_list = ecco_podaac_s3_open(ShortName="ECCO_L4_TEMP_SALINITY_LLC0090GRID_MONTHLY_V4R4",\
-                                StartDate="2010-01",EndDate="2010-12")
+                                StartDate=sd,EndDate=ed)
 
-        ds = xr.open_mfdataset(file_list, engine='h5netcdf', \
+        ds = xr.open_mfdataset(file_list, engine=engine, \
                        data_vars='minimal',coords='minimal',\
                        compat='override', parallel=True,
                        decode_cf=False,)
 
+        et = time.time()
+        print(f'[dask] {et-st}')
         client.close()
 
     if sys.argv[1] == 'disk':
+        st = time.time()
 
         ecco_podaac_s3_get(ShortName="ECCO_L4_TEMP_SALINITY_LLC0090GRID_MONTHLY_V4R4",\
-                   StartDate="2010-01",EndDate="2010-12",\
-                   n_workers=1)
+                   StartDate=sd,EndDate=ed,\
+                   n_workers=1, download_root_dir='/tmp')
 
-        ds = xr.open_mfdataset("/home/jovyan/Downloads/ECCO_V4r4_PODAAC/ECCO_L4_TEMP_SALINITY_LLC0090GRID_MONTHLY_V4R4/*.nc",\
+        ds = xr.open_mfdataset("/tmp/ECCO_L4_TEMP_SALINITY_LLC0090GRID_MONTHLY_V4R4/*.nc",\
                        data_vars='minimal',coords='minimal',\
-                       compat='override',)
+                       compat='override', decode_cf=False, engine=engine)
+        et = time.time()
+        print(f'[disk] {et-st}')
